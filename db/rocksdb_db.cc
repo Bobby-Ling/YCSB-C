8d25ed7b6ab222fed4ac90e195e91ca6c4ce954a
HUSTJiang <1911713425@qq.com>
2025-03-27 04:21:13 +0000

    add hdr

--- db/rocksdb_db.cc
+++ db/rocksdb_db.cc
@@ -17,18 +17,6 @@ using namespace std;
 
 namespace ycsbc {
 
-    void RocksDB::latency_hiccup(uint64_t iops) {
-        //fprintf(f_hdr_hiccup_output_, "mean     95th     99th     99.99th   IOPS");
-        fprintf(f_hdr_hiccup_output_, "%-11.2lf %-8ld %-8ld %-8ld %-8ld\n",
-              hdr_mean(hdr_last_1s_),
-              hdr_value_at_percentile(hdr_last_1s_, 95),
-              hdr_value_at_percentile(hdr_last_1s_, 99),
-              hdr_value_at_percentile(hdr_last_1s_, 99.99),
-			  iops);
-        hdr_reset(hdr_last_1s_);
-        fflush(f_hdr_hiccup_output_);
-    }
-
     RocksDB::RocksDB(const char *dbfilename, utils::Properties &props) :noResult(0){
         int r = hdr_init(1,INT64_C(3600000000),3,&hdr_);
         r |= hdr_init(1, INT64_C(3600000000), 3, &hdr_last_1s_);
@@ -88,9 +76,9 @@ namespace ycsbc {
 
         ////
 
-        // int dboption = stoi(props["dboption"]);
+        int dboption = stoi(props["dboption"]);
 
-        // if ( dboption == 1) {  //use cloud db
+        if ( dboption == 1) {  //use cloud db
             std::string kDBPath = dbfilename;
             std::string kBucketSuffix = "generalbuckets-jx";
             std::string kRegion = "ap-northeast-1";
@@ -133,10 +121,10 @@ namespace ycsbc {
             options->upload_while_generate = true;
             options->cloud_move=true;
             options->db_paths = {{kDBPath + "/ebs", 1024l * 1024 * 1024 * 1024},{kDBPath + "/s3", 1024l * 1024 * 1024 * 1024}};
-        // }
-        // else if ( dboption == 2 ) { //
+        }
+        else if ( dboption == 2 ) { //
 
-        // }
+        }
         
     }
 
@@ -208,45 +196,45 @@ namespace ycsbc {
         db_->GetProperty("rocksdb.stats",&stats);
         cout<<stats<<endl;
  
-	cout << "-----------------------------------------------------" << endl;
-	cout << "SUMMARY latency (us) of this run with HDR measurement" << endl;
-	cout << "         ALL      GET      PUT      UPD      SCAN    RMW" << endl;
-	fprintf(stdout, "mean     %-8.3lf %-8.3lf %-8.3lf %-8.3lf %8.3lf %8.3lf\n",
-		hdr_mean(hdr_),
-		hdr_mean(hdr_get_),
-		hdr_mean(hdr_put_),
-		hdr_mean(hdr_update_),
-		hdr_mean(hdr_scan_),
-		hdr_mean(hdr_rmw_));
-		
-	fprintf(stdout, "95th     %-8ld %-8ld %-8ld %-8ld %-8ld %-8ld\n",
-		hdr_value_at_percentile(hdr_, 95),
-    		hdr_value_at_percentile(hdr_get_, 95),
-		hdr_value_at_percentile(hdr_put_, 95),
-		hdr_value_at_percentile(hdr_update_, 95),
-		hdr_value_at_percentile(hdr_scan_, 95),
-		hdr_value_at_percentile(hdr_rmw_, 95));
-        fprintf(stdout, "99th     %-8ld %-8ld %-8ld %-8ld %-8ld %-8ld\n",
-                hdr_value_at_percentile(hdr_, 99),
-                hdr_value_at_percentile(hdr_get_, 99),
-                hdr_value_at_percentile(hdr_put_, 99),
-                hdr_value_at_percentile(hdr_update_, 99),
-		hdr_value_at_percentile(hdr_scan_, 99),
-		hdr_value_at_percentile(hdr_rmw_, 99));
-        fprintf(stdout, "99.99th  %-8ld %-8ld %-8ld %-8ld %-8ld %-8ld\n",
-                hdr_value_at_percentile(hdr_, 99.99),
-                hdr_value_at_percentile(hdr_get_, 99.99),
-                hdr_value_at_percentile(hdr_put_, 99.99),
-                hdr_value_at_percentile(hdr_update_, 99.99),
-                hdr_value_at_percentile(hdr_scan_, 99.99),
-                hdr_value_at_percentile(hdr_rmw_, 99.99));
+        cout << "-----------------------------------------------------" << endl;
+        cout << "SUMMARY latency (us) of this run with HDR measurement" << endl;
+        cout << "         ALL      GET      PUT      UPD      SCAN    RMW" << endl;
+        fprintf(stdout, "mean     %-8.3lf %-8.3lf %-8.3lf %-8.3lf %8.3lf %8.3lf\n",
+            hdr_mean(hdr_),
+            hdr_mean(hdr_get_),
+            hdr_mean(hdr_put_),
+            hdr_mean(hdr_update_),
+            hdr_mean(hdr_scan_),
+            hdr_mean(hdr_rmw_));
+            
+        fprintf(stdout, "95th     %-8ld %-8ld %-8ld %-8ld %-8ld %-8ld\n",
+            hdr_value_at_percentile(hdr_, 95),
+                hdr_value_at_percentile(hdr_get_, 95),
+            hdr_value_at_percentile(hdr_put_, 95),
+            hdr_value_at_percentile(hdr_update_, 95),
+            hdr_value_at_percentile(hdr_scan_, 95),
+            hdr_value_at_percentile(hdr_rmw_, 95));
+            fprintf(stdout, "99th     %-8ld %-8ld %-8ld %-8ld %-8ld %-8ld\n",
+                    hdr_value_at_percentile(hdr_, 99),
+                    hdr_value_at_percentile(hdr_get_, 99),
+                    hdr_value_at_percentile(hdr_put_, 99),
+                    hdr_value_at_percentile(hdr_update_, 99),
+            hdr_value_at_percentile(hdr_scan_, 99),
+            hdr_value_at_percentile(hdr_rmw_, 99));
+            fprintf(stdout, "99.99th  %-8ld %-8ld %-8ld %-8ld %-8ld %-8ld\n",
+                    hdr_value_at_percentile(hdr_, 99.99),
+                    hdr_value_at_percentile(hdr_get_, 99.99),
+                    hdr_value_at_percentile(hdr_put_, 99.99),
+                    hdr_value_at_percentile(hdr_update_, 99.99),
+                    hdr_value_at_percentile(hdr_scan_, 99.99),
+                    hdr_value_at_percentile(hdr_rmw_, 99.99));
 
-	
-	int ret = hdr_percentiles_print(hdr_,f_hdr_output_,5,1.0,CLASSIC);
-	if( 0 != ret ){
-	    cout << "hdr percentile output print file error!" <<endl;
-	}
-	cout << "-------------------------------" << endl;
+        
+        int ret = hdr_percentiles_print(hdr_,f_hdr_output_,5,1.0,CLASSIC);
+        if( 0 != ret ){
+            cout << "hdr percentile output print file error!" <<endl;
+        }
+	    cout << "-------------------------------" << endl;
     }
 
     bool RocksDB::HaveBalancedDistribution() {
@@ -268,26 +256,38 @@ namespace ycsbc {
     }
 
     void RocksDB::RecordTime(int op,uint64_t tx_xtime){
-    	if(tx_xtime > 3600000000) {
-	    cout << "too large tx_xtime" << endl;
-	}
+            if(tx_xtime > 3600000000) {
+            cout << "too large tx_xtime" << endl;
+        }
 
-	hdr_record_value(hdr_, tx_xtime);
-	hdr_record_value(hdr_last_1s_, tx_xtime);
+        hdr_record_value(hdr_, tx_xtime);
+        hdr_record_value(hdr_last_1s_, tx_xtime);
 
-	if(op == 1){
-		hdr_record_value(hdr_put_, tx_xtime);
-	} else if(op == 2) {
-		hdr_record_value(hdr_get_, tx_xtime);
-	} else if(op == 3) {
-		hdr_record_value(hdr_update_, tx_xtime);
-	} else if(op == 4) {
-		hdr_record_value(hdr_scan_, tx_xtime);
-	} else if(op == 5) {
-		hdr_record_value(hdr_rmw_, tx_xtime);
-	} else {
-		cout << "record time err with op error" << endl;
-	}
+        if(op == 1){
+            hdr_record_value(hdr_put_, tx_xtime);
+        } else if(op == 2) {
+            hdr_record_value(hdr_get_, tx_xtime);
+        } else if(op == 3) {
+            hdr_record_value(hdr_update_, tx_xtime);
+        } else if(op == 4) {
+            hdr_record_value(hdr_scan_, tx_xtime);
+        } else if(op == 5) {
+            hdr_record_value(hdr_rmw_, tx_xtime);
+        } else {
+            cout << "record time err with op error" << endl;
+        }
+    }
+
+    void RocksDB::latency_hiccup(uint64_t iops) {
+        //fprintf(f_hdr_hiccup_output_, "mean     95th     99th     99.99th   IOPS");
+        fprintf(f_hdr_hiccup_output_, "%-11.2lf %-8ld %-8ld %-8ld %-8ld\n",
+              hdr_mean(hdr_last_1s_),
+              hdr_value_at_percentile(hdr_last_1s_, 95),
+              hdr_value_at_percentile(hdr_last_1s_, 99),
+              hdr_value_at_percentile(hdr_last_1s_, 99.99),
+			  iops);
+        hdr_reset(hdr_last_1s_);
+        fflush(f_hdr_hiccup_output_);
     }
 
     void RocksDB::SerializeValues(std::vector<KVPair> &kvs, std::string &value) {
--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ sudo rm -rf /tmp/rocksdb-test/*
 
 date
 
-./ycsbc -dbstatistics true -db rocksdb -dbpath /tmp/rocksdb-test -threads 4 -P ./workloads/workloada.spec -load true | tee load`date +%Y%m%d%H%M%S`.log
+./ycsbc -db rocksdb -dbpath /tmp/rocksdb-test -threads 4 -P ./workloads/workloada.spec -load true -dboption 1 -dbstatistics true| tee load`date +%Y%m%d%H%M%S`.log
 
 date
 
-./ycsbc -db rocksdb -dbpath /tmp/rocksdb-test -threads 4 -P ./workloads/workloada.spec -dbstatistics true -run true| tee run`date +%Y%m%d%H%M%S`.log
+./ycsbc -db rocksdb -dbpath /tmp/rocksdb-test -threads 4 -P ./workloads/workloada.spec -run true -dboption 1 -dbstatistics true| tee run`date +%Y%m%d%H%M%S`.log
 
 date
